b2faea884046e558fbe5ec98633e2fc8f2e5a927
Michael Asiedu <michaelasiedu148@gmail.com>
2024-12-23 12:19:47 +0000

    feat(directory):Add CLI and initialization support with `clap`
    Introduced `clap` dependency and implemented a CLI for the "Time Machine" tool, including an `init` subcommand to initialize directories. Added `initialize_directory` function to create the necessary `.timemachine` folder and accompanying unit tests for validation.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("File system time machine!");
+use clap::{Parser};
+use timemachine;
+
+#[derive(Parser)]
+#[command(name = "timemachine", version = "0.1.0", author = "Michael Asiedu")]
+struct Cli {
+    #[command(subcommand)]
+    command:Commands
 }
+
+#[derive(clap::Subcommand)]
+enum Commands {
+    Init {
+        #[arg(value_name="DIRECTORY")]
+        dir:String,
+    }
+}
+fn main() {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init { dir} => {
+            match timemachine::initialize_directory(dir) {
+                Ok(_) => eprintln!("Initialization complete! {}", dir),
+                Err(e) => eprintln!("Initialization failed {:?}", e),
+
+            }
+        }
+    }
+
+}
+
