85cdf3229f1a8f3dca4b297102863e72fa4e28e7
Michael Asiedu <michaelasiedu148@gmail.com>
2024-12-23 15:24:38 +0000

    feat(snapshot):Add snapshot functionality to Time Machine
    Introduced functionality to take directory snapshots, including initialization and metadata management. Added tests to ensure proper snapshot creation and metadata updates. Updated dependencies in Cargo.toml and Cargo.lock to include required libraries for timestamping and temporary directory management.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
-use clap::{Parser};
+use clap::Parser;
 use timemachine;
 
 #[derive(Parser)]
 #[command(name = "timemachine", version = "0.1.0", author = "Michael Asiedu")]
 struct Cli {
     #[command(subcommand)]
-    command:Commands
+    command: Commands,
 }
 
 #[derive(clap::Subcommand)]
 enum Commands {
     Init {
-        #[arg(value_name="DIRECTORY")]
-        dir:String,
-    }
+        #[arg(value_name = "DIRECTORY")]
+        dir: String,
+    },
+    Snapshot {
+        #[arg(value_name = "DIRECTORY")]
+        dir: String,
+    },
 }
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Init { dir} => {
-            match timemachine::initialize_directory(dir) {
-                Ok(_) => eprintln!("Initialization complete with metatada! {}", dir),
-                Err(e) => eprintln!("Initialization failed {:?}", e),
-
-            }
+        Commands::Init { dir } => match timemachine::initialize_directory(dir) {
+            Ok(_) => eprintln!("Initialization complete with metatada! {}", dir),
+            Err(e) => eprintln!("Initialization failed {:?}", e),
+        },
+        Commands::Snapshot {dir} => match timemachine::take_snapshot(dir) {
+            Ok(_) => eprintln!("Snapshot taken succesfully!"),
+            Err(e) => eprintln!("Snapshot failed")
         }
     }
-
 }
-
