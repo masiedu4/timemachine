8695857d14f5355a85c6b959f20182eec2d46bf7
Michael Asiedu <michaelasiedu148@gmail.com>
2024-12-23 12:55:15 +0000

    feat(metadata):Add metadata initialization
    Introduced `metadata.json` creation in the `.timemachine` directory to track snapshots. Added `serde` and `serde_json` dependencies for serializing/deserializing metadata. Updated tests to validate metadata initialization.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
 use std::fs;
+use std::io::Write;
 use std::path::Path;
+use serde_json;
+use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize,Debug)]
+struct SnapshotMetadata {
+    snapshots:Vec<Snapshot>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Snapshot {
+    id:usize,
+    timestamp:String,
+    changes:usize,
+}
 /// Initializes the given directory for Time Machine by creating a `.timemachine` folder.
 ///
 /// # Arguments
@@ -12,10 +26,29 @@ pub fn initialize_directory(path: &str) -> Result<(), std::io::Error> {
     let metadata_dir = Path::new(path).join(".timemachine");
 
     if !metadata_dir.exists() {
-        fs::create_dir_all(metadata_dir)?;
+        fs::create_dir_all(&metadata_dir)?;
+    }
+
+    // initialize metadata
+    initialize_metadata(&metadata_dir)?;
+
+    Ok(())
+}
+
+pub fn initialize_metadata(path:&Path) -> Result<(), std::io::Error> {
+    let metadata_path = path.join("metadata.json");
+
+    // check if metadata.json exists, if not create an empty one with an empty snapshot json
+
+    if !metadata_path.exists() {
+        let metadata = SnapshotMetadata {snapshots:Vec::new()};
+        let metadata_content = serde_json::to_string(&metadata)?;
+        let mut file = fs::File::create(metadata_path)?;
+        file.write_all(metadata_content.as_bytes())?;
     }
 
     Ok(())
+
 }
 
 #[cfg(test)]
@@ -27,7 +60,7 @@ mod tests {
 
     #[test]
 
-    fn test_initialize_directory() {
+    fn test_initialize_metadata_directory()  {
         let test_dir = "./test_dir";
 
         if Path::new(test_dir).exists() {
@@ -36,10 +69,13 @@ mod tests {
 
         initialize_directory(test_dir).unwrap();
 
-        // Check if .timemachine directory exists
-        let metadata_dir = Path::new(test_dir).join(".timemachine");
+        // Check if metadata.json exists
+        let metadata_path = Path::new(test_dir).join(".timemachine").join("metadata.json");
 
-        assert!(metadata_dir.exists());
+        assert!(metadata_path.exists());
+
+        let metadata_content = fs::read_to_string(metadata_path).unwrap();
+        assert_eq!(metadata_content, r#"{"snapshots":[]}"#);
 
         // clean up
         fs::remove_dir_all(test_dir).unwrap();
